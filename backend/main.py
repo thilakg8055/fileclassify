8e85bee0aa91b32d66e9945928c59e4c64ffbeed
Thilak-Goovo <Thilak.G@goovo.in>
2026-06-18 10:39:11 +0530

    deployment1

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="FileClassify API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://fileclassify.vercel.app/login" , "https://fileclassify.vercel.app/admin"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
